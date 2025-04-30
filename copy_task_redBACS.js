afb8a6dae272ca501c517c4f21f2d9cf7e439799
William Ngiam <william.ngiam@adelaide.edu.au>
2025-04-30 20:14:45 +0930

    update demo instructions

--- copy_task_redBACS.js
+++ copy_task_redBACS.js
@@ -15,7 +15,10 @@
  **/
 
 /* Initialize jsPsych */
-var jsPsych = initJsPsych();
+var jsPsych = initJsPsych({
+    show_progress_bar: true,
+    auto_update_progress_bar: false
+});
 
 // Set-up DataPipe (in future, auto-upload to OSF)
 // generate a random subject ID with 10 characters
@@ -28,12 +31,15 @@ jsPsych.data.addProperties({
 });
 
 // DEFINE EXPERIMENT VARIABLES
-const n_trials = 30; // Number of trials in the experiment, should be even
+const n_trials = 60; // Number of trials in the experiment, should be even
 const n_items = 6; // Number of items in the copy grid
 const n_stimuli = 2 // No colour versus colour
 var stimuli = ["BACS", "colourBACS"]
 
 // EXPERIMENT STIMULI
+// Colours (for practice trial)
+const colours = ['colours/blue.png', 'colours/green.png','colours/red.png','colours/yellow.png']
+
 // Brussels Artificial Character Set (BACS)
 const BACS = ['bacs/BACS_J.png', 'bacs/BACS_R.png', 'bacs/BACS_X.png', 'bacs/BACS_K.png',
     'bacs/BACS_P.png', 'bacs/BACS_G.png', 'bacs/BACS_V.png', 'bacs/BACS_B.png'
@@ -85,29 +91,106 @@ timeline_interleaved.push(preload);
 var welcome = {
     type: jsPsychHtmlButtonResponse,
     stimulus: `
-    <p>Welcome to the experiment.</p>
+    <p>Welcome to the demo.</p>
     <br>
 `,
     choices: [">>"],
     button_html: ['<button class="jspsych-btn">%choice%</button>']
 };
 timeline_interleaved.push(welcome);
-//timeline_baselineFirst.push(welcome);
-//timeline_conjunctionFirst.push(welcome);
 
-/* define instructions trial */
-var instructions = {
+/* show task instructions */
+var instructions_1 = {
     type: jsPsychHtmlButtonResponse,
     stimulus: `
-    <p>Instructions go here.</p>
+    <img src="instructions/show_model_grid.png" width=800 height=240>
+    <p>On each trial, you will start by seeing a model grid on the left. Your task is to recreate this grid on the right.</p>
+    <p>When your mouse cursor hovers over the model grid (on the left), the model grid will be visible.</p>
+    <p>However, when your mouse cursor moves over to the right, the model grid will no longer be visible.</p>
+    <p>You may return and check the model grid as you wish.</p>
+`,
+    choices: ['>>'],
+    button_html: ['<button class="jspsych-btn">%choice%</button>']
+};
+timeline_interleaved.push(instructions_1);
+
+var instructions_2 = {
+    type: jsPsychHtmlButtonResponse,
+    stimulus: `
+    <img src="instructions/show_resource_grid.png" width-800 height=240>
+    <p>To recreate the model grid, drag and drop items from the resource pool on the right.</p>
+    <p>If you drop the correct item in the correct location, it will lock into place.</p>
+    <p>If you drop the wrong item or drop an item in the wrong location, it will wriggle before returning to the pool.</p>
+    <p>A trial is completed when the model grid has been successfully recreated.</p>
+    <p>There will be one practice trial before you begin the experiment.</p>
     <p>Press the button below when you are ready.</p>
 `,
     choices: ['>>'],
     button_html: ['<button class="jspsych-btn">%choice%</button>']
 };
-timeline_interleaved.push(instructions);
-//timeline_baselineFirst.push(instructions);
-//timeline_conjunctionFirst.push(instructions);
+timeline_interleaved.push(instructions_2);
+
+/* practice trial */
+// CREATE GRIDS
+// programmatically create a model grid (4 by 4) with 6 items
+var practice_rows = 4
+var practice_cols = 4
+var practice_n_items = 4
+
+// create resource grid and select items
+var resource_grid_contents = [[colours[0], colours[1], colours[2], colours[3]]]
+var selected_model_items = jsPsych.randomization.sampleWithoutReplacement(colours, practice_n_items)
+// select unique (sample WITHOUT replacement) positions in the model grid to place the items in
+var selected_model_grid_indices = jsPsych.randomization.sampleWithoutReplacement([...Array((practice_rows * practice_cols)).keys()], practice_n_items)
+
+var model_grid_contents = []
+
+for (var r = 0; r < practice_rows; r++) {
+    model_grid_contents[r] = []
+    for (var c = 0; c < practice_cols; c++) {
+        let index = r * practice_cols + c
+        if (selected_model_grid_indices.includes(index)) {
+            model_grid_contents[r][c] = selected_model_items.pop()
+        } else {
+            model_grid_contents[r][c] = null
+        }
+    }
+}
+
+var start_practice = {
+    type: jsPsychHtmlButtonResponse,
+    stimulus: "",
+    choices: ["Start practice trial"],
+    button_html: ['<button class="jspsych-btn" style="position:relative; right:325px">%choice%</button>'],
+}
+timeline_interleaved.push(start_practice)
+
+var practice_trial = {
+    type: jsPsychCopyingTask,
+    model_grid_contents: model_grid_contents,
+    resource_grid_contents: resource_grid_contents,
+    canvas_width: 1300,
+    item_file_type: "img",
+    prompt: 'Copy the left grid onto the right grid',
+}
+
+timeline_interleaved.push(practice_trial)
+
+/* start experiment instructions */
+var start_experiment = {
+    type: jsPsychHtmlButtonResponse,
+    stimulus: `
+    <p>You have now completed the practice trial.</p>
+    <p>There are 60 trials in total.</p>
+    <p>The experiment will begin on the next trial.</p>
+`,
+    choices: ['>>'],
+    button_html: ['<button class="jspsych-btn">%choice%</button>'],
+    on_start: function() {
+        jsPsych.progressBar.progress = 0
+    }
+};
+timeline_interleaved.push(start_experiment);
 
 // TRIAL LOOP
 // Interleaved trials
